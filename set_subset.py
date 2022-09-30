25acbee887090f46e4b3b773ca7ac7d3c3985105
Prathmesh Churhe <105632156+PrathmeshChurhe@users.noreply.github.com>
2022-09-30 22:44:57 +0530

    Delete set_subset.py

--- set_subset.py
+++
@@ -1,8 +0,0 @@
-# determine whether the set A is subset of set B using input() method
-
-# optimal method for problem
-
-for i in range(int(input())):
-  a=int(input()); A=set(input().split())
-  b=int(input()); B=set(input().split())
-  print(a.issubset(b))
